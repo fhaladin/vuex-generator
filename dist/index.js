b8a36c709f7215fa785aedb266c9bd72bb0a3430
fhaladin <farhan.nif.alaudin@gmail.com>
2022-12-20 14:11:36 +0800

    Major version update | v2 initial update

--- dist/index.js
+++ dist/index.js
@@ -1,363 +1,196 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _sprintfJs = require('sprintf-js');
+
 var _constantCase = require('constant-case');
-
-var _mutationMap = require('./mutation-map');
-
-var _mutationMap2 = _interopRequireDefault(_mutationMap);
-
-var _axios = require('./axios');
-
-var _axios2 = _interopRequireDefault(_axios);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+var objectPath = require('object-path');
+
 var VuexGenerator = function () {
-  /**
-   * Predefined endpoint
-   * @param {Object | String} endpoint 
-   * 
-   * Use string for single endpoint
-   * Use object for multiple endpoint with 'base' param as base endpoint
-   * 
-   */
   function VuexGenerator() {
     _classCallCheck(this, VuexGenerator);
 
     this.state = {};
     this.mutations = {};
-    this.axios = new _axios2.default();
+    this.actions = {};
   }
 
-  // ANCHOR - Mutation
-
-
   _createClass(VuexGenerator, [{
-    key: 'createAsync',
-    value: function createAsync(apiEndpoint) {
+    key: 'createState',
+    value: function createState(states) {
       var _this = this;
 
-      var endpoint = apiEndpoint.endpoint,
-          _apiEndpoint$type = apiEndpoint.type,
-          type = _apiEndpoint$type === undefined ? '' : _apiEndpoint$type;
-      var mutations = this.mutations;
+      var _loop = function _loop(key) {
+        var mutations = _this.mutations;
 
+        var KEY = (0, _constantCase.constantCase)(key);
 
-      if ((typeof apiEndpoint === 'undefined' ? 'undefined' : _typeof(apiEndpoint)) === 'object' && !Array.isArray(apiEndpoint) && apiEndpoint !== null) {
-        this.axios.endpoint[type || 'base'] = endpoint;
-      } else {
-        this.axios.endpoint.base = apiEndpoint;
-      }
+        mutations['SET_' + KEY] = function (state, payload) {
+          state[key] = payload;
+        };
 
-      mutations[(0, _mutationMap2.default)(type).PENDING] = function (state, payload) {
-        _this.setState(state, {
-          key: payload.loadingKey || 'loading',
-          value: payload.loading
-        });
+        mutations['RESET_' + KEY] = function (state) {};
+
+        _this.state[key] = states[key];
       };
 
-      mutations[(0, _mutationMap2.default)(type).SUCCESS] = function (state, payload) {
-        if (payload.fetch) {
-          _this.setState(state, {
-            key: payload.dataKey || 'data',
-            value: payload.data
-          });
-        }
-
-        _this.setState(state, {
-          key: payload.statusCodeKey || 'statusCode',
-          value: payload.statusCode
-        });
-
-        _this.setState(state, {
-          key: payload.loadingKey || 'loading',
-          value: false
-        });
-      };
-
-      mutations[(0, _mutationMap2.default)(type).FAILURE] = function (state, payload) {
-        _this.setState(state, {
-          key: payload.statusCodeKey || 'statusCode',
-          value: payload.statusCode
-        });
-
-        _this.setState(state, {
-          key: payload.loadingKey || 'loading',
-          value: false
-        });
-      };
-    }
-  }, {
-    key: 'createNonAsync',
-    value: function createNonAsync(states) {
       for (var key in states) {
-        if (typeof states[key] === 'function') {
-          var custom = states[key]();
+        _loop(key);
+      }
+    }
+  }, {
+    key: 'createAsync',
+    value: async function createAsync(name, _ref) {
+      var axios = _ref.axios,
+          endpoint = _ref.endpoint,
+          context = _ref.context,
+          _ref$payload = _ref.payload,
+          payload = _ref$payload === undefined ? {} : _ref$payload,
+          _ref$state = _ref.state,
+          state = _ref$state === undefined ? '' : _ref$state,
+          _ref$fetch = _ref.fetch,
+          fetch = _ref$fetch === undefined ? false : _ref$fetch,
+          _ref$cached = _ref.cached,
+          cached = _ref$cached === undefined ? false : _ref$cached,
+          _ref$source = _ref.source,
+          source = _ref$source === undefined ? null : _ref$source,
+          _ref$method = _ref.method,
+          method = _ref$method === undefined ? 'get' : _ref$method,
+          _ref$loadingDefault = _ref.loadingDefault,
+          loadingDefault = _ref$loadingDefault === undefined ? false : _ref$loadingDefault;
+      var commit = context.commit,
+          vuexState = context.state;
 
-          for (var customKey in custom) {
-            var _custom$customKey = custom[customKey],
-                value = _custom$customKey.default,
-                _custom$customKey$arr = _custom$customKey.arrayMutations,
-                arrayMutations = _custom$customKey$arr === undefined ? false : _custom$customKey$arr,
-                _custom$customKey$obj = _custom$customKey.objectMutations,
-                objectMutations = _custom$customKey$obj === undefined ? false : _custom$customKey$obj;
 
+      var refresh = !!payload.refresh;
+      var sprintfData = payload.sprintfData || {};
+      var payloadData = payload.data || {};
+      var config = payload.config || {};
 
-            this.createBaseMutations(customKey, value);
+      var trueEndpoint = (0, _sprintfJs.sprintf)(endpoint, sprintfData);
 
-            if (arrayMutations) {
-              if (!Array.isArray(arrayMutations)) {
-                this.createArrayMutations(customKey);
-              } else {
-                var _iteratorNormalCompletion = true;
-                var _didIteratorError = false;
-                var _iteratorError = undefined;
+      var loadingKey = state ? state + 'Loading' : 'loading';
+      var statusCodeKey = state ? state + 'StatusCode' : 'statusCode';
+      var stateKey = state ? state + 'Data' : 'data';
 
-                try {
-                  for (var _iterator = arrayMutations[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                    var type = _step.value;
+      this.state[loadingKey] = loadingDefault;
 
-                    this.createArrayMutations(customKey, type);
-                  }
-                } catch (err) {
-                  _didIteratorError = true;
-                  _iteratorError = err;
-                } finally {
-                  try {
-                    if (!_iteratorNormalCompletion && _iterator.return) {
-                      _iterator.return();
-                    }
-                  } finally {
-                    if (_didIteratorError) {
-                      throw _iteratorError;
-                    }
-                  }
-                }
-              }
-            }
+      if (!refresh) {
+        var data = vuexState[stateKey];
 
-            if (objectMutations) {
-              if (!Array.isArray(arrayMutations)) {
-                this.createObjectMutations(customKey);
-              } else {
-                var _iteratorNormalCompletion2 = true;
-                var _didIteratorError2 = false;
-                var _iteratorError2 = undefined;
-
-                try {
-                  for (var _iterator2 = arrayMutations[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-                    var _type = _step2.value;
-
-                    this.createObjectMutations(customKey, _type);
-                  }
-                } catch (err) {
-                  _didIteratorError2 = true;
-                  _iteratorError2 = err;
-                } finally {
-                  try {
-                    if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                      _iterator2.return();
-                    }
-                  } finally {
-                    if (_didIteratorError2) {
-                      throw _iteratorError2;
-                    }
-                  }
-                }
-              }
-            }
+        // Array
+        if (Array.isArray(data)) {
+          if (cached && data.length > 0) {
+            return;
           }
-        } else {
-          this.createBaseMutations(key, states[key]);
+        }
+
+        // Object
+        if (cached && (typeof data === 'undefined' ? 'undefined' : _typeof(data)) === 'object' && data !== null) {
+          if (cached && Object.keys(data).length > 0) {
+            return;
+          }
         }
       }
-    }
-  }, {
-    key: 'createBaseMutations',
-    value: function createBaseMutations(key, value) {
-      var mutations = this.mutations;
 
-      var KEY = (0, _constantCase.constantCase)(key);
+      commit(MUTATION_MAP(name).PENDING, {
+        loadingKey: loadingKey,
+        loading: true
+      });
 
-      mutations['SET_' + KEY] = function (state, payload) {
-        state[key] = payload;
-      };
+      try {
+        var axiosArguments = method === 'get' ? [trueEndpoint, config] : [trueEndpoint, payloadData, config];
 
-      mutations['RESET_' + KEY] = function (state) {
-        state[key] = value;
-      };
+        var response = await axios[method].apply(axios, axiosArguments);
+        var _data = source ? objectPath.get(response, source) : response.data;
 
-      this.state[key] = value;
-    }
+        commit(MUTATION_MAP(name).SUCCESS, {
+          fetch: fetch,
+          statusCodeKey: statusCodeKey,
+          statusCode: response.status,
+          stateKey: stateKey,
+          loadingKey: loadingKey,
+          data: _data
+        });
 
-    // ANCHOR - ARRAY
+        return response;
+      } catch (error) {
+        commit(MUTATION_MAP(name).FAILURE, {
+          loadingKey: loadingKey,
+          statusCode: error.response.status
+        });
 
-  }, {
-    key: 'createArrayMutations',
-    value: function createArrayMutations(key) {
-      var type = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
-
-      switch (type) {
-        case 'pop':
-          return this.createPop(key);
-        case 'push':
-          return this.createPush(key);
-        case 'flat':
-          return this.createFlat(key);
-        case 'fill':
-          return this.createFill(key);
-        case 'slice':
-          return this.createSlice(key);
-        case 'shift':
-          return this.createShift(key);
-        case 'splice':
-          return this.createSplice(key);
-        case 'unshift':
-          return this.createUnshift(key);
-
-        default:
-          this.createPop(key);
-          this.createPush(key);
-          this.createFlat(key);
-          this.createFill(key);
-          this.createSlice(key);
-          this.createShift(key);
-          this.createSplice(key);
-          this.createUnshift(key);
-          break;
+        throw error;
       }
     }
   }, {
-    key: 'createPush',
-    value: function createPush(key) {
-      this.mutations['PUSH_' + key.toUpperCase()] = function (state, payload) {
-        state[key].push(payload);
-      };
-    }
-  }, {
-    key: 'createPop',
-    value: function createPop(key) {
-      this.mutations['POP_' + key.toUpperCase()] = function (state) {
-        state[key].pop();
-      };
-    }
-  }, {
-    key: 'createSlice',
-    value: function createSlice(key) {
-      this.mutations['SLICE_' + key.toUpperCase()] = function (state, payload) {
-        state[key].slice(payload.start, payload.end);
-      };
-    }
-  }, {
-    key: 'createSplice',
-    value: function createSplice(key) {
-      this.mutations['SPLICE_' + key.toUpperCase()] = function (state, payload) {
-        var _state$key;
+    key: 'generateAsyncMutations',
+    value: function generateAsyncMutations(name) {
+      var state = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
 
-        var _items = payload.items || undefined;
-        var items = Array.isArray(_items) ? _items : [_items];
+      var mutations = {};
 
-        (_state$key = state[key]).splice.apply(_state$key, [payload.start, payload.deleteCount].concat(_toConsumableArray(items)));
-      };
-    }
-  }, {
-    key: 'createShift',
-    value: function createShift(key) {
-      this.mutations['SHIFT_' + key.toUpperCase()] = function (state) {
-        state[key].shift();
-      };
-    }
-  }, {
-    key: 'createUnshift',
-    value: function createUnshift(key) {
-      this.mutations['UNSHIFT_' + key.toUpperCase()] = function (state, payload) {
-        var _state$key2;
+      var loadingKey = state ? state + 'Loading' : 'loading';
+      var stateKey = state ? state + 'Data' : 'data';
+      var arrStateKey = (0, _constantCase.constantCase)(stateKey).split('_');
 
-        var items = Array.isArray(payload) ? payload : [payload];
-        (_state$key2 = state[key]).unshift.apply(_state$key2, _toConsumableArray(items));
-      };
-    }
-  }, {
-    key: 'createFlat',
-    value: function createFlat(key) {
-      this.mutations['FLAT_' + key.toUpperCase()] = function (state, payload) {
-        state[key].flat(payload);
-      };
-    }
-  }, {
-    key: 'createFill',
-    value: function createFill(key) {
-      this.mutations['FILL_' + key.toUpperCase()] = function (state, payload) {
-        state[key].fill(payload.value, payload.start, payload.end);
-      };
-    }
-
-    // ANCHOR - OBJECT
-
-  }, {
-    key: 'createObjectMutations',
-    value: function createObjectMutations(key) {
-      var type = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
-
-      switch (type) {
-        case 'add':
-          return this.createAdd(key);
-        case 'update':
-          return this.createUpdate(key);
-        case 'remove':
-          return this.createRemove(key);
-
-        default:
-          this.createAdd(key);
-          this.createUpdate(key);
-          this.createRemove(key);
-          break;
+      if (arrStateKey.length > 1) {
+        arrStateKey.pop();
       }
-    }
-  }, {
-    key: 'createAdd',
-    value: function createAdd(key) {
-      this.mutations['ADD_' + key.toUpperCase()] = function (state, payload) {
-        state[key][payload.key] = payload.value;
-      };
-    }
-  }, {
-    key: 'createUpdate',
-    value: function createUpdate(key) {
-      this.mutations['UPDATE_' + key.toUpperCase()] = function (state, payload) {
-        state[key][payload.key] = payload.value;
-      };
-    }
-  }, {
-    key: 'createRemove',
-    value: function createRemove(key) {
-      this.mutations['REMOVE_' + key.toUpperCase()] = function (state, payload) {
-        state[key][payload.key] = undefined;
-      };
-    }
-  }, {
-    key: 'setState',
-    value: function setState(state, _ref) {
-      var key = _ref.key,
-          value = _ref.value;
 
-      if (typeof window !== 'undefined') {
-        window.$nuxt.$set(state, key, value);
-      } else {
-        state[key] = value;
-      }
+      var normalizedStateKey = arrStateKey.join('_');
+
+      mutations['RESET_' + normalizedStateKey] = function (state) {
+        state[stateKey] = null;
+      };
+
+      mutations['SET_LOADING_' + normalizedStateKey] = function (state, payload) {
+        state[loadingKey || 'loading'] = payload;
+      };
+
+      mutations[MUTATION_MAP(name).PENDING] = function (state, payload) {
+        state[payload.loadingKey || 'loading'] = true;
+      };
+
+      mutations[MUTATION_MAP(name).SUCCESS] = function (state, payload) {
+        if (payload.fetch) {
+          state[payload.stateKey || 'data'] = payload.data;
+        }
+
+        state[payload.statusCodeKey || 'statusCode'] = payload.statusCode;
+        state[payload.loadingKey || 'loading'] = false;
+      };
+
+      mutations[MUTATION_MAP(name).FAILURE] = function (state, payload) {
+        state[payload.statusCodeKey || 'statusCode'] = payload.statusCode;
+        state[payload.loadingKey || 'loading'] = false;
+      };
+
+      return mutations;
     }
   }]);
 
   return VuexGenerator;
 }();
 
-module.exports = VuexGenerator;
+var MUTATION_MAP = function MUTATION_MAP(type) {
+  var _type = type ? type + '_' : '';
+
+  return {
+    PENDING: _type + 'PENDING',
+    SUCCESS: _type + 'SUCCESS',
+    FAILURE: _type + 'FAILURE'
+  };
+};
+
+exports.default = VuexGenerator;
--- src/index.js
+++ src/index.js
@@ -1,245 +1,160 @@
+import { sprintf } from 'sprintf-js'
 import { constantCase } from 'constant-case'
 
-import mMap from './mutation-map'
-import Axios from './axios'
+const objectPath = require('object-path')
 
 class VuexGenerator {
-  /**
-   * Predefined endpoint
-   * @param {Object | String} endpoint 
-   * 
-   * Use string for single endpoint
-   * Use object for multiple endpoint with 'base' param as base endpoint
-   * 
-   */
   constructor () {
     this.state = {}
     this.mutations = {}
-    this.axios = new Axios()
+    this.actions = {}
   }
 
-  // ANCHOR - Mutation
-  createAsync (apiEndpoint) {
-    const { endpoint, type = '' } = apiEndpoint
-    const { mutations } = this
-
-    if (typeof apiEndpoint === 'object' && !Array.isArray(apiEndpoint) && apiEndpoint !== null) {
-      this.axios.endpoint[type || 'base'] = endpoint
-    } else {
-      this.axios.endpoint.base = apiEndpoint
-    }
-    
-    mutations[mMap(type).PENDING] = (state, payload) => {
-      this.setState(state, {
-        key: payload.loadingKey || 'loading',
-        value: payload.loading
-      })
-    }
-
-    mutations[mMap(type).SUCCESS] = (state, payload) => {
-      if (payload.fetch) {
-        this.setState(state, {
-          key: payload.dataKey || 'data',
-          value: payload.data
-        })
-      }
-      
-      this.setState(state, {
-        key: payload.statusCodeKey || 'statusCode',
-        value: payload.statusCode
-      })
-
-      this.setState(state, {
-        key: payload.loadingKey || 'loading',
-        value: false
-      })
-    }
-
-    mutations[mMap(type).FAILURE] = (state, payload) => {
-      this.setState(state, {
-        key: payload.statusCodeKey || 'statusCode',
-        value: payload.statusCode
-      })
-
-      this.setState(state, {
-        key: payload.loadingKey || 'loading',
-        value: false
-      })
-    }
-  }
-
-  createNonAsync (states) {
+  createState (states) {
     for (const key in states) {
-      if (typeof states[key] === 'function') {
-        const custom = states[key]()
+      const { mutations } = this
+      const KEY = constantCase(key)
 
-        for (const customKey in custom) {
-          const {
-            default: value,
-            arrayMutations = false,
-            objectMutations = false
-          } = custom[customKey]
+      mutations[`SET_${KEY}`] = (state, payload) => {
+        state[key] = payload
+      }
 
-          this.createBaseMutations(customKey, value)
+      mutations[`RESET_${KEY}`] = (state) => {
+      }
 
-          if (arrayMutations) {
-            if (!Array.isArray(arrayMutations)) {
-              this.createArrayMutations(customKey)
-            } else {
-              for (const type of arrayMutations) {
-                this.createArrayMutations(customKey, type)
-              }
-            }
-          }
+      this.state[key] = states[key]
+    }
+  }
 
-          if (objectMutations) {
-            if (!Array.isArray(arrayMutations)) {
-              this.createObjectMutations(customKey)
-            } else {
-              for (const type of arrayMutations) {
-                this.createObjectMutations(customKey, type)
-              }
-            }
-          }
+  async createAsync (name, {
+    axios,
+    endpoint,
+    context,
+    payload = {},
+    state = '',
+    fetch = false,
+    cached = false,
+    source = null,
+    method = 'get',
+    loadingDefault = false
+  }) {
+    const { commit, state: vuexState } = context
+
+    const refresh = !!payload.refresh
+    const sprintfData = payload.sprintfData || {}
+    const payloadData = payload.data || {}
+    const config = payload.config || {}
+
+    const trueEndpoint = sprintf(endpoint, sprintfData)
+
+    const loadingKey = state ? `${state}Loading` : 'loading'
+    const statusCodeKey = state ? `${state}StatusCode` : 'statusCode'
+    const stateKey = state ? `${state}Data` : 'data'
+
+    this.state[loadingKey] = loadingDefault
+
+    if (!refresh) {
+      const data = vuexState[stateKey]
+
+      // Array
+      if (Array.isArray(data)) {
+        if (cached && data.length > 0) {
+          return
         }
-      } else {
-        this.createBaseMutations(key, states[key])
+      }
+
+      // Object
+      if (cached && typeof data === 'object' && data !== null) {
+        if (cached && Object.keys(data).length > 0) {
+          return
+        }
       }
     }
-  }
 
-  createBaseMutations (key, value) {
-    const { mutations } = this
-    const KEY = constantCase(key)
+    commit(MUTATION_MAP(name).PENDING, {
+      loadingKey,
+      loading: true
+    })
 
-    mutations[`SET_${KEY}`] = (state, payload) => {
-      state[key] = payload
-    }
+    try {
+      const axiosArguments = method === 'get'
+        ? [trueEndpoint, config]
+        : [trueEndpoint, payloadData, config]
 
-    mutations[`RESET_${KEY}`] = (state) => {
-      state[key] = value
-    }
+      const response = await axios[method](...axiosArguments)
+      const data = source ? objectPath.get(response, source) : response.data
 
-    this.state[key] = value
-  }
+      commit(MUTATION_MAP(name).SUCCESS, {
+        fetch,
+        statusCodeKey,
+        statusCode: response.status,
+        stateKey,
+        loadingKey,
+        data
+      })
 
-  // ANCHOR - ARRAY
-  createArrayMutations (key, type = null) {
-    switch (type) {
-      case 'pop': return this.createPop(key)
-      case 'push': return this.createPush(key)
-      case 'flat': return this.createFlat(key)
-      case 'fill': return this.createFill(key)
-      case 'slice': return this.createSlice(key)
-      case 'shift': return this.createShift(key)
-      case 'splice': return this.createSplice(key)
-      case 'unshift': return this.createUnshift(key)
+      return response
+    } catch (error) {
+      commit(MUTATION_MAP(name).FAILURE, {
+        loadingKey,
+        statusCode: error.response.status
+      })
 
-      default:
-        this.createPop(key)
-        this.createPush(key)
-        this.createFlat(key)
-        this.createFill(key)
-        this.createSlice(key)
-        this.createShift(key)
-        this.createSplice(key)
-        this.createUnshift(key)
-        break
+      throw error
     }
   }
 
-  createPush (key) {
-    this.mutations[`PUSH_${key.toUpperCase()}`] = (state, payload) => {
-      state[key].push(payload)
+  generateAsyncMutations (name, state = null) {
+    const mutations = {}
+
+    const loadingKey = state ? `${state}Loading` : 'loading'
+    const stateKey = state ? `${state}Data` : 'data'
+    const arrStateKey = constantCase(stateKey).split('_')
+
+    if (arrStateKey.length > 1) {
+      arrStateKey.pop()
     }
-  }
 
-  createPop (key) {
-    this.mutations[`POP_${key.toUpperCase()}`] = (state) => {
-      state[key].pop()
+    const normalizedStateKey = arrStateKey.join('_')
+
+    mutations[`RESET_${normalizedStateKey}`] = (state) => {
+      state[stateKey] = null
     }
-  }
 
-  createSlice (key) {
-    this.mutations[`SLICE_${key.toUpperCase()}`] = (state, payload) => {
-      state[key].slice(payload.start, payload.end)
+    mutations[`SET_LOADING_${normalizedStateKey}`] = (state, payload) => {
+      state[loadingKey || 'loading'] = payload
     }
-  }
 
-  createSplice (key) {
-    this.mutations[`SPLICE_${key.toUpperCase()}`] = (state, payload) => {
-      const _items = payload.items || undefined
-      const items = Array.isArray(_items) ? _items : [_items]
-
-      state[key].splice(payload.start, payload.deleteCount, ...items)
+    mutations[MUTATION_MAP(name).PENDING] = (state, payload) => {
+      state[payload.loadingKey || 'loading'] = true
     }
-  }
 
-  createShift (key) {
-    this.mutations[`SHIFT_${key.toUpperCase()}`] = (state) => {
-      state[key].shift()
+    mutations[MUTATION_MAP(name).SUCCESS] = (state, payload) => {
+      if (payload.fetch) {
+        state[payload.stateKey || 'data'] = payload.data
+      }
+
+      state[payload.statusCodeKey || 'statusCode'] = payload.statusCode
+      state[payload.loadingKey || 'loading'] = false
     }
-  }
 
-  createUnshift (key) {
-    this.mutations[`UNSHIFT_${key.toUpperCase()}`] = (state, payload) => {
-      const items = Array.isArray(payload) ? payload : [payload]
-      state[key].unshift(...items)
+    mutations[MUTATION_MAP(name).FAILURE] = (state, payload) => {
+      state[payload.statusCodeKey || 'statusCode'] = payload.statusCode
+      state[payload.loadingKey || 'loading'] = false
     }
-  }
 
-  createFlat (key) {
-    this.mutations[`FLAT_${key.toUpperCase()}`] = (state, payload) => {
-      state[key].flat(payload)
-    }
-  }
-
-  createFill (key) {
-    this.mutations[`FILL_${key.toUpperCase()}`] = (state, payload) => {
-      state[key].fill(payload.value, payload.start, payload.end)
-    }
-  }
-
-  // ANCHOR - OBJECT
-  createObjectMutations (key, type = null) {
-    switch (type) {
-      case 'add': return this.createAdd(key)
-      case 'update': return this.createUpdate(key)
-      case 'remove': return this.createRemove(key)
-
-      default:
-        this.createAdd(key)
-        this.createUpdate(key)
-        this.createRemove(key)
-        break
-    }
-  }
-
-  createAdd (key) {
-    this.mutations[`ADD_${key.toUpperCase()}`] = (state, payload) => {
-      state[key][payload.key] = payload.value
-    }
-  }
-
-  createUpdate (key) {
-    this.mutations[`UPDATE_${key.toUpperCase()}`] = (state, payload) => {
-      state[key][payload.key] = payload.value
-    }
-  }
-
-  createRemove (key) {
-    this.mutations[`REMOVE_${key.toUpperCase()}`] = (state, payload) => {
-      state[key][payload.key] = undefined
-    }
-  }
-
-  setState (state, { key, value }) {
-    if (typeof window !== 'undefined') {
-      window.$nuxt.$set(state, key, value)
-    } else {
-      state[key] = value
-    }
+    return mutations
   }
 }
 
-module.exports = VuexGenerator
+const MUTATION_MAP = (type) => {
+  const _type = type ? `${type}_` : ''
+
+  return {
+    PENDING: `${_type}PENDING`,
+    SUCCESS: `${_type}SUCCESS`,
+    FAILURE: `${_type}FAILURE`
+  }
+}
+
+export default VuexGenerator
